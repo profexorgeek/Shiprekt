8e1c7943cc078ad345db8021f223aad5120ca004
Justin Johnson <justin.d.johnson@gmail.com>
2019-09-05 17:59:48 -0600

    Added tileset file

---
+++ GlobalContent/Levels/ShiprektTiles.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="ShiprektTiles" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="../shiprekt.png" width="2048" height="2048"/>
+</tileset>
