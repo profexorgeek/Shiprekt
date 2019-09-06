29c295b5da707874fc19972ec5346e29f3326fe1
Adam Patridge <patridge@gmail.com>
2019-09-06 13:47:28 -0600

    Initial birbs

--- Shiprekt/Screens/GameScreen.cs
+++ Shiprekt/Screens/GameScreen.cs
@@ -11,27 +11,27 @@ using FlatRedBall.Graphics.Animation;
 using FlatRedBall.Graphics.Particle;
 using FlatRedBall.Math.Geometry;
 using FlatRedBall.Localization;
-
-
+using Shiprekt.Factories;
+using Shiprekt.Entities;
 
 namespace Shiprekt.Screens
 {
-	public partial class GameScreen
-	{
+    public partial class GameScreen
+    {
 
-		void CustomInitialize()
-		{
+        void CustomInitialize()
+        {
             DummyShip.InitializeRacingInput(InputManager.Xbox360GamePads[0]);
             DummyShip.TeamIndex = 1;
 
             FlatRedBallServices.Game.IsMouseVisible = true;
 
             OffsetTilemapLayers();
-		}
+        }
 
         private void OffsetTilemapLayers()
         {
-            foreach(var layer in Map.MapLayers)
+            foreach (var layer in Map.MapLayers)
             {
                 var property = layer.Properties.FirstOrDefault(item => item.Name == "PositionZ");
                 var floatValue = layer.RelativeZ;
@@ -46,18 +46,18 @@ namespace Shiprekt.Screens
         }
 
         void CustomActivity(bool firstTimeCalled)
-		{
+        {
             Camera.Main.X = Ship1.X;
             Camera.Main.Y = Ship1.Y;
 
+            DoBirdSpawning();
+        }
 
-		}
-
-		void CustomDestroy()
-		{
+        void CustomDestroy()
+        {
 
 
-		}
+        }
 
         static void CustomLoadStaticContent(string contentManagerName)
         {
@@ -65,5 +65,17 @@ namespace Shiprekt.Screens
 
         }
 
-	}
+        void DoBirdSpawning()
+        {
+            if (BirdList.Count <= BirdCount)
+            {
+                var x = FlatRedBallServices.Random.Between(0, Map.Width);
+                var y = FlatRedBallServices.Random.Between(0, -Map.Height);
+                var altitude = FlatRedBallServices.Random.Between(Bird.MinBirdAltitude, Bird.MaxBirdAltitude);
+                var bird = BirdFactory.CreateNew(x, y);
+                bird.Altitude = altitude;
+            }
+        }
+
+    }
 }
