d7a28e698b2161a3e413ba576940ae17c3ae1f39
GorillaOne <gorillaone@gorillatactics.com>
2019-09-07 20:05:44 -0400

    Added ship health (Defined in the ShipValues.csv. The variable is just to track current health.

--- Shiprekt/TileGraphics/TiledMapSave.Serialization.cs
+++ Shiprekt/TileGraphics/TiledMapSave.Serialization.cs
@@ -464,7 +464,7 @@ namespace TMXGlueLib
 
 
         /// <remarks/>
-        [XmlAttribute("width")]
+        [XmlAttribute()]
         public int width
         {
             get;
@@ -472,7 +472,7 @@ namespace TMXGlueLib
         }
 
         /// <remarks/>
-        [XmlAttribute("height")]
+        [XmlAttribute()]
         public int height
         {
             get;
