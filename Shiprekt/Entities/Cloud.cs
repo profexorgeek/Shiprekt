b7e68db61e317692dbffca9c792bfaad40294507
Adam Patridge <patridge@gmail.com>
2019-09-08 22:26:21 -0600

    First shot at clouds

--- Shiprekt/Entities/Cloud.cs
+++ Shiprekt/Entities/Cloud.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using FlatRedBall;
@@ -8,38 +8,57 @@ using FlatRedBall.AI.Pathfinding;
 using FlatRedBall.Graphics.Animation;
 using FlatRedBall.Graphics.Particle;
 using FlatRedBall.Math.Geometry;
+using Microsoft.Xna.Framework;
 
 namespace Shiprekt.Entities
 {
-	public partial class Cloud
-	{
+    public partial class Cloud
+    {
+        public bool Animating
+        {
+            get => CloudSprite.Animate;
+            set
+            {
+                CloudSprite.Animate = value;
+                CloudShadowSprite.Animate = value;
+            }
+        }
+        public float Altitude
+        {
+            get => CloudSprite.RelativeZ;
+            set
+            {
+                CloudSprite.RelativeZ = value;
+            }
+        }
+
         /// <summary>
         /// Initialization logic which is execute only one time for this Entity (unless the Entity is pooled).
         /// This method is called when the Entity is added to managers. Entities which are instantiated but not
         /// added to managers will not have this method called.
         /// </summary>
-		private void CustomInitialize()
-		{
+        private void CustomInitialize()
+        {
+            Velocity.X = (float)Math.Cos(RotationZ);
+            Velocity.Y = (float)Math.Sin(RotationZ);
+        }
+
+        private void CustomActivity()
+        {
 
 
-		}
+        }
 
-		private void CustomActivity()
-		{
-
-
-		}
-
-		private void CustomDestroy()
-		{
+        private void CustomDestroy()
+        {
 
 
-		}
+        }
 
         private static void CustomLoadStaticContent(string contentManagerName)
         {
 
 
         }
-	}
+    }
 }
