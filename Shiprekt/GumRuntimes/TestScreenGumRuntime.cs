05e82b5a0e354f4786aa15fd2dc610d0a7310764
Victor Chelaru <VicChelaru@gmail.com>
2019-09-09 15:59:06 -0600

    Added Gum.

---
+++ Shiprekt/GumRuntimes/TestScreenGumRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shiprekt.GumRuntimes
+{
+    public partial class TestScreenGumRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
