7ce581a86b827a8c19a2c8855f83d195297b45f9
Victor Chelaru <VicChelaru@gmail.com>
2019-09-15 09:54:21 -0600

    Added spawn point entity, not yet functional.

--- Shiprekt/Content/GlobalContent/Levels/ShiprektTiles.tsx
+++ Shiprekt/Content/GlobalContent/Levels/ShiprektTiles.tsx
@@ -1,6 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="ShiprektTiles" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
  <image source="../shiprekt.png" width="2048" height="2048"/>
+ <tile id="388" type="SpawnPoint"/>
  <tile id="451">
   <objectgroup draworder="index">
    <object id="1" x="5" y="26">
