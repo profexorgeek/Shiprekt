add94b5d079905b0b2cf0a1da437de56ac9622a2
Adam Patridge <patridge@gmail.com>
2019-09-16 20:28:10 -0600

    First round of shot miss effect

--- Shiprekt/Entities/Bullet.cs
+++ Shiprekt/Entities/Bullet.cs
@@ -8,33 +8,36 @@ using FlatRedBall.AI.Pathfinding;
 using FlatRedBall.Graphics.Animation;
 using FlatRedBall.Graphics.Particle;
 using FlatRedBall.Math.Geometry;
+using Shiprekt.Utilities;
+using Shiprekt.Factories;
 
 namespace Shiprekt.Entities
 {
-	public partial class Bullet
-	{
+    public partial class Bullet
+    {
         public int TeamIndex { get; set; }
+        
 
         /// <summary>
         /// Initialization logic which is execute only one time for this Entity (unless the Entity is pooled).
         /// This method is called when the Entity is added to managers. Entities which are instantiated but not
         /// added to managers will not have this method called.
         /// </summary>
-		private void CustomInitialize()
-		{
-			InitializeArc();			
-		}
+        private void CustomInitialize()
+        {
+            InitializeArc();
+        }
 
-		private void CustomActivity()
-		{
-			CannonballSpriteInstance.RelativeYVelocity = Math.Max(-100, CannonballSpriteInstance.RelativeYVelocity);
-		}
+        private void CustomActivity()
+        {
+            CannonballSpriteInstance.RelativeYVelocity = Math.Max(-100, CannonballSpriteInstance.RelativeYVelocity);
+        }
 
-		private void CustomDestroy()
-		{
+        private void CustomDestroy()
+        {
 
 
-		}
+        }
 
         private static void CustomLoadStaticContent(string contentManagerName)
         {
@@ -42,18 +45,19 @@ namespace Shiprekt.Entities
 
         }
 
-		private void InitializeArc()
-		{
-			CannonballSpriteInstance.RelativeYVelocity = 100;
-			var time = BulletDistance / BulletSpeed;
-			CannonballSpriteInstance.RelativeYAcceleration = -(CannonballSpriteInstance.RelativeYVelocity / time) * 2;
-		}
-
-		internal void HitSurface()
+        private void InitializeArc()
         {
-			Instructions.Clear(); 
+            CannonballSpriteInstance.RelativeYVelocity = 100;
+            var time = BulletDistance / BulletSpeed;
+            CannonballSpriteInstance.RelativeYAcceleration = -(CannonballSpriteInstance.RelativeYVelocity / time) * 2;
+        }
 
-            // todo - add particles
+        internal void HitSurface()
+        {
+            Instructions.Clear();
+
+            ShotMissEffectFactory.CreateNew().TriggerEffect(X, Y, RotationZ);
+
             // broadcast this so that a collision can occur at screen level
             Destroy();
         }
---
+++ Shiprekt/Entities/Effects/ShotMissEffect.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlatRedBall;
+using FlatRedBall.Input;
+using FlatRedBall.Instructions;
+using FlatRedBall.AI.Pathfinding;
+using FlatRedBall.Graphics.Animation;
+using FlatRedBall.Graphics.Particle;
+using FlatRedBall.Math.Geometry;
+using Shiprekt.Utilities;
+
+namespace Shiprekt.Entities.Effects
+{
+    public partial class ShotMissEffect
+    {
+        // If we ever get more than dirt/water, this might need to be an enum system.
+        public bool IsDirtMiss { get; set; } = false;
+        const string dirtMissChainName = "SmokeParticles";
+        const string WaterMissChainName = "WakeParticle2";
+        float lifeSecondsRemaining;
+        EasyEmitter shotMissEmitter;
+
+        private void CustomInitialize()
+        {
+            lifeSecondsRemaining = EffectDurationSeconds;
+            shotMissEmitter = EasyEmitter.BuildExplosion(GlobalContent.EffectChains[WaterMissChainName], EmitterPower.Medium, 20f);
+            shotMissEmitter.AttachTo(this, false);
+            shotMissEmitter.RelativeZ = -2f;
+            shotMissEmitter.RelativeX = -4f;
+        }
+
+        private void CustomActivity()
+        {
+            shotMissEmitter.TimedEmit();
+            DoLifeCountdown();
+        }
+
+        private void CustomDestroy()
+        {
+
+
+        }
+
+        private static void CustomLoadStaticContent(string contentManagerName)
+        {
+
+
+        }
+
+        public void TriggerEffect(float x, float y, float rotation)
+        {
+            X = x;
+            Y = y;
+            RotationZ = rotation;
+
+            shotMissEmitter.Emit();
+        }
+
+
+        void DoLifeCountdown()
+        {
+            lifeSecondsRemaining -= TimeManager.SecondDifference;
+
+            if (lifeSecondsRemaining <= 0)
+            {
+                Destroy();
+            }
+        }
+
+        void DoEffectManagement()
+        {
+
+        }
+    }
+}
